1deb11013915fcc4bbd7c9c28de3b9611ba2b8b6
Ben Carey <bncarey42@users.noreply.github.com>
2022-07-22 10:43:45 -0500

    WIP

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
-import requests
 from src.parser import Parser
 
 if __name__ == '__main__':
-    raw = response = requests.get(url)
-    data = json.load()
+    with open('./resume.json') as fr:
+        data = json.load(fr)
+        print(data)
     parser = Parser(data=data)
+    parser.parse()
